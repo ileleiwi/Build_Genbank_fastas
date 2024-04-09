790b2d2377b0350a23f5f58f426e2d9e3876eeb9
Ikaia <ileleiwi@gmail.com>
2024-04-09 09:15:47 -0600

    added timeout to download script and updated driver script

--- download_genbank.py
+++ download_genbank.py
@@ -17,19 +17,6 @@ local_dir = args.local_dir
 error_log_file = os.path.join(local_dir, "ftp_errors.log")
 logging.basicConfig(filename=error_log_file, level=logging.ERROR)
 
-class MyFTPHost(ftputil.FTPHost):
-    def _log(self, cmd, result):
-        if result.startswith("4") or result.startswith("5"):
-            logging.error("FTP command failed: %s", cmd)
-            logging.error("Error message: %s", result)
-        else:
-            print("Sent:", cmd)
-            print("Received:", result)
-
-
-# FTP server details
-ftp_host = "ftp.ncbi.nlm.nih.gov"
-ftp_dir = "/genomes/genbank/bacteria/"
 
 # Genome Log file
 genome_log = local_dir + "/genome.log"
@@ -109,76 +96,90 @@ def write_metadata(assembly, genbank_name, gtdb_taxonomy):
         with open(tsv_file, "a") as f:
             f.write(f"{assembly}\t{genbank_name}\t{gtdb_taxonomy}\n")
 
+
+class MyFTPHost(ftputil.FTPHost):
+    def _log(self, cmd, result):
+        if result.startswith("4") or result.startswith("5"):
+            logging.error("FTP command failed: %s", cmd)
+            logging.error("Error message: %s", result)
+        else:
+            print("Sent:", cmd)
+            print("Received:", result)
+
+# FTP server details
+ftp_host = "ftp.ncbi.nlm.nih.gov"
+ftp_dir = "/genomes/genbank/bacteria/"
+
 # Connect to server
-ftp = MyFTPHost(ftp_host, "anonymous", "anonymous@")
-ftp.use_passive_mode = True
-ftp.chdir(ftp_dir)
-print("FTP connection successful.")
-files = ftp.listdir(ftp.curdir)
-for item in files:
-    if item not in read_log_file(log_type="genome"):
-        if ftp.path.isdir(item):
-            print("Working on:", item)
-            ftp.chdir(item)
-            if ftp.path.isdir("latest_assembly_versions"):
-                ftp.chdir("latest_assembly_versions")
-                dir_files = ftp.listdir(ftp.curdir)
-                len_dir = len(dir_files)
-                print(len_dir)
-                item_check = 0
-                for file in dir_files:
-                    if file not in read_log_file(log_type="subdir_dl"):
-                        if not ftp.path.exists(file):
-                            logging.error("Directory does not exist: %s", file)
-                            item_check += 1
-                            ftp.chdir(ftp_dir + item + "/latest_assembly_versions")
-                            continue
-                        ftp.chdir(file)
-                        ftp.download("md5checksums.txt", md5_file)
-                        file_name_fna = file + "_genomic.fna.gz"
-                        file_name_gtf = file + "_genomic.gtf.gz"
-                        genomic_fna_check = 0
-                        genomic_gtf_check = 0
-                        # Download genomic.gtf.gz
-                        try:
-                            if file_name_gtf in ftp.listdir(ftp.curdir) and file_name_gtf not in read_log_file(log_type="success"):
-                                local_path_gtf = os.path.join(local_dir, file_name_gtf)
-                                ftp.download_if_newer(file_name_gtf, local_path_gtf)
-                                genomic_gtf_check += 1
-                                write_log_file(log_type="success", file_name=file_name_gtf)
-                                # Download genomic.fna.gz
-                                if file_name_fna in ftp.listdir(ftp.curdir) and read_log_file(log_type="success"):
-                                    local_path_fna = os.path.join(local_dir, file_name_fna)
-                                    ftp.download_if_newer(file_name_fna, local_path_fna)
-                                    genomic_fna_check += 1
-                                    write_log_file(log_type="success", file_name=file_name_fna)
-                                    
-                            else:
-                                print(f"{file} has no gtf file")
-                                write_log_file(log_type="subdir_dl", file_name=file)
-                                item_check += 1
-                                ftp.chdir(ftp_dir + item + "/latest_assembly_versions") 
-                                continue
-                        except ftputil.error.FTPOSError:
-                                logging.error("FTPOSError occurred for file: %s. Skipping...", file)
-                                time.sleep(5)
-                                continue
-                        if genomic_fna_check + genomic_gtf_check == 2:
-                            print(f"{file} Files Downloaded")
-                            if verify_checksums(local_path_gtf, file + "_genomic.gtf.gz") and verify_checksums(local_path_fna, file + "_genomic.fna.gz"):                            
-                                write_log_file(log_type="subdir_dl", file_name=file)
-                                write_metadata(assembly=file, genbank_name=item, gtdb_taxonomy="")
+with MyFTPHost(ftp_host, "anonymous", "anonymous@", timeout=60) as ftp:
+    ftp.use_passive_mode = True
+    ftp.chdir(ftp_dir)
+    print("FTP connection successful.")
+    files = ftp.listdir(ftp.curdir)
+    for item in files:
+        if item not in read_log_file(log_type="genome"):
+            if ftp.path.isdir(item):
+                print("Working on:", item)
+                ftp.chdir(item)
+                if ftp.path.isdir("latest_assembly_versions"):
+                    ftp.chdir("latest_assembly_versions")
+                    dir_files = ftp.listdir(ftp.curdir)
+                    len_dir = len(dir_files)
+                    print(len_dir)
+                    item_check = 0
+                    for file in dir_files:
+                        if file not in read_log_file(log_type="subdir_dl"):
+                            if not ftp.path.exists(file):
+                                logging.error("Directory does not exist: %s", file)
                                 item_check += 1
                                 ftp.chdir(ftp_dir + item + "/latest_assembly_versions")
-                            else:
-                                break
-                            
-        if item_check == len(dir_files):
-            write_log_file(log_type="genome", file_name=item)  
-            ftp.close()
-            break          
-    #ftp.chdir(ftp_dir)  
-            print("--------------------")  
+                                continue
+                            ftp.chdir(file)
+                            ftp.download("md5checksums.txt", md5_file)
+                            file_name_fna = file + "_genomic.fna.gz"
+                            file_name_gtf = file + "_genomic.gtf.gz"
+                            genomic_fna_check = 0
+                            genomic_gtf_check = 0
+                            # Download genomic.gtf.gz
+                            try:
+                                if file_name_gtf in ftp.listdir(ftp.curdir) and file_name_gtf not in read_log_file(log_type="success"):
+                                    local_path_gtf = os.path.join(local_dir, file_name_gtf)
+                                    ftp.download_if_newer(file_name_gtf, local_path_gtf)
+                                    genomic_gtf_check += 1
+                                    write_log_file(log_type="success", file_name=file_name_gtf)
+                                    # Download genomic.fna.gz
+                                    if file_name_fna in ftp.listdir(ftp.curdir) and read_log_file(log_type="success"):
+                                        local_path_fna = os.path.join(local_dir, file_name_fna)
+                                        ftp.download_if_newer(file_name_fna, local_path_fna)
+                                        genomic_fna_check += 1
+                                        write_log_file(log_type="success", file_name=file_name_fna)
+                                        
+                                else:
+                                    print(f"{file} has no gtf file")
+                                    write_log_file(log_type="subdir_dl", file_name=file)
+                                    item_check += 1
+                                    ftp.chdir(ftp_dir + item + "/latest_assembly_versions") 
+                                    continue
+                            except ftputil.error.FTPOSError:
+                                    logging.error("FTPOSError occurred for file: %s. Skipping...", file)
+                                    time.sleep(5)
+                                    continue
+                            if genomic_fna_check + genomic_gtf_check == 2:
+                                print(f"{file} Files Downloaded")
+                                if verify_checksums(local_path_gtf, file + "_genomic.gtf.gz") and verify_checksums(local_path_fna, file + "_genomic.fna.gz"):                            
+                                    write_log_file(log_type="subdir_dl", file_name=file)
+                                    write_metadata(assembly=file, genbank_name=item, gtdb_taxonomy="")
+                                    item_check += 1
+                                    ftp.chdir(ftp_dir + item + "/latest_assembly_versions")
+                                else:
+                                    break
+                                
+            if item_check == len(dir_files):
+                write_log_file(log_type="genome", file_name=item)  
+                ftp.close()
+                break          
+        #ftp.chdir(ftp_dir)  
+                print("--------------------")  
 
 
 
--- driver_script.sh
+++ driver_script.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+#set env variables
+DOWNLOAD_DIR=$1
+THREADS=$2
+
+#downlaod fasta and gtf files
+python download_data.py DOWNLOAD_DIR
+
+#unzip files
+bash unzip_files.sh "$THREADS" "$DOWNLOAD_DIR"
+
+#pull 16S sequences
