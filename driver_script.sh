0c4276d9c7e2725bf2488e91dc4f5f1a3ea4fb2e
Ikaia <ileleiwi@gmail.com>
2024-04-12 11:38:49 -0600

    updated driver script

--- driver_script.sh
+++ driver_script.sh
@@ -6,6 +6,14 @@ THREADS=$2
 #downlaod fasta and gtf files
 python download_data.py "$DOWNLOAD_DIR"
 
+#extract lineage
+for file in "$DOWNLOAD_DIR"/*_ani_report.txt; do
+    python extract_lineage.py "$DOWNLOAD_DIR"/genbank_assembly_metadata.tsv "$file"
+    echo "Processing file: $file"
+done
+
+
+
 #unzip files
 bash unzip_files.sh "$THREADS" "$DOWNLOAD_DIR"
 
